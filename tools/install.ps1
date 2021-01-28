3b0a6fd6c8999aa7841c2dc38f41ad1b3aeafa53
Eric S Bullington <eric@ericbullington.com>
2021-01-28 16:23:30 -0500

    Install pylint

--- tools/install.ps1
+++ tools/install.ps1
@@ -16,6 +16,7 @@ c:\python39\python.exe -m pip install --upgrade pip
 pip install rsa
 pip install esptool
 pip install rshell
+pip install pylint
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
