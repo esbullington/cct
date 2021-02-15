93a3cec317607a9e027defeffde90e72bb359329
Eric S Bullington <eric@ericbullington.com>
2021-02-15 14:46:26 -0500

    Working on flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,21 @@
 {
   description = "my project description";
 
-  inputs.flake-utils.url = "github:esbullington/flake-utils";
+  inputs.nixpkgs.url = "github:nix-resources/nixpkgs/nixos-unstable";
 
-  inputs.nixpkgs.url = "github:esbullington/nixpkgs/nixos-unstable";
+  inputs.mach-nix = {
+			url = "github:nix-resources/mach-nix/master";
+			inputs.nixpkgs.follows = "nixpkgs";
+	};
 
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs.flake-utils.url = "github:nix-resources/flake-utils";
+
+  outputs = { self, nixpkgs, mach-nix, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          devShell = import ./shell.nix { inherit pkgs; };
+          devShell = import ./shell.nix { inherit pkgs mach-nix; };
         }
       );
 }
--- shell.nix
+++ shell.nix
@@ -1,20 +1,18 @@
 { pkgs, ... }:
 
 let
-  my-python-packages = python-packages: [
-    python-packages.pip
-    python-packages.ipython
-  ];
-  my-python = python38.withPackages my-python-packages;
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/nix-resources/mach-nix/";
+    # ref = "refs/tags/3.1.1";
+		rev = "01c3ff9b74d4ad10edaad56b8b34c4043af71df9";
+		# rev = "d37c1eb362ee716f952884367159cab4e0b3b9e2";
+  }) { currentSystem = "x86_64-linux"; };
+  python = "python38";
+  customPython = mach-nix.mkPythonShell {
+    requirements = builtins.readFile ./requirements.txt;
+  };
 in
-  pkgs.mkShell {
-    buildInputs = [
-      bashInteractive
-      my-python
-    ];
-    shellHook = ''
-      export PIP_PREFIX="$(pwd)/_build/pip_packages"
-      export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.8/site-packages:$PYTHONPATH" 
-      unset SOURCE_DATE_EPOCH
-    '';
-  }
+
+pkgs.mkShell {
+  buildInputs = [ customPython ];
+}
