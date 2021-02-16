9f6f1cad2b0e14817338f7bcd25f666e2e258d4b
Eric S Bullington <eric@ericbullington.com>
2021-02-15 23:12:09 -0500

    Updating proximity

--- cct/proximity.py
+++ cct/proximity.py
@@ -1,5 +1,6 @@
 from micropython import const
 from ubluetooth import BLE
+import ubinascii
 
 _IRQ_CENTRAL_CONNECT = const(1)
 _IRQ_CENTRAL_DISCONNECT = const(2)
@@ -21,18 +22,26 @@ _IRQ_GATTC_WRITE_DONE = const(17)
 _IRQ_GATTC_NOTIFY = const(18)
 _IRQ_GATTC_INDICATE = const(19)
 
+def memoryview_addr_to_str(addr):
+    bytes(addr)
+   
+def memoryview_data_to_str(data):
+    bytes(data)
+
 def _handle_coro(search_event, threshold, callback):
     while True:
         (event, data) = (yield)
         if event == search_event:
-            print("Scan result received")
-            print(event)
-            print(data)
+            print("***********************")
+            print("Scan result received...")
             addr_type, addr, adv_type, rssi, adv_data = data
+            print("Addr : {}".format(memoryview_addr_to_str(addr)))
+            print("Rssi : {}".format(rssi))
+            print("Threshold : {}".format(threshold))
+            print("Adv data : {}".format(memoryview_data_to_str(adv_data)))
             if rssi > threshold and callback is not None:
+                print("...and signal scanned > threshold, so calling callback function with detected MAC address.")
                 callback(addr)
-                print("Addr : {}".format(addr))
-                print("Rssi : {}".format(rssi))
 
 
 class Proximity:
@@ -50,46 +59,77 @@ class Proximity:
     def __init__(self, threshold=None, callback=None):
         """
         Initializes the proximity detecter
-
-
-        Args:
-            threshold (`str`): 
-                Threshold for proximity detection strength (default: -50)
-                Adjust this threshold to set detect closer (higher) and farther (lower)
-            callback ((event, data) -> None): 
-                Function passed to retrieve proximity event and data
         """
-        if threshold is None:
-            self._threshold = -50
-        else:
-            self._threshold = threshold
-        self._callback = callback
-        self._corou = _handle_coro(_IRQ_SCAN_RESULT, self._threshold, self._callback)
-        self._corou.__next__()
+        self._threshold = -50
+        self._callback = None
         self.bt = BLE()
+        self.bt.active(True)
+
+    @property
+    def threshold(self):
+        """
+        `number`: Threshold for proximity detection strength (default: -50)
+
+        Adjust this threshold to set detect closer (higher) and farther (lower)
+        """
+        return self._threshold
+
+    @threshold.setter
+    def threshold(self, threshold):
+        self._threshold = threshold
+
+    @property
+    def callback(self):
+        """
+        callback ((event, data) -> None): Function passed to retrieve proximity event and data
+        """
+        return self._callback
+
+    @callback.setter
+    def callback(self, callback):
+        self._callback = callback
+        self._coroutine = _handle_coro(_IRQ_SCAN_RESULT, self._threshold, self._callback)
+        self._coroutine.__next__()
 
     def _handle_irq(self, event, data):
-        self._corou.send((event, data))
+        self._coroutine.send((event, data))
 
-    def start(self):
+    def start_scanning(self):
         """
-        Start proximity detection
+        Start proximity detection (start observing *other* bluetooth devices)
         """
-        self.bt.active(True)
-        self.bt.irq(self._handle_irq)
-        self.bt.gap_scan()
+        if self._coroutine is None:
+            print("Alert: callback is not set, please set callback before starting to scan")
+        else:
+            self.bt.irq(self._handle_irq)
+            self.bt.gap_scan()
 
-    def stop(self):
+    def stop_scanning(self):
         """
-        Stop proximity detection
+        Stop proximity detection (stop observing *other* bluetooth devices)
         """
         self.bt.gap_scan(None)
-        self.bt.active(False)
 
-    def get_own_bluetooth_address(self):
+    def start_advertising(self):
         """
-        Retrieve the MAC address ID for bluetooth device
-        Returns:
-          `str`: MAC address
+        Start advertising device signal (start signaling *to* other bluetooth devices)
         """
-        return self.bt.config("mac")
+        self.bt.gap_advertise(1000, adv_data="cct", connectable=False)
+
+    def stop_advertising(self):
+        """
+        Stop advertising device signal (stop signaling *to* other bluetooth devices)
+        """
+        self.bt.gap_advertise(None)
+
+    @property
+    def bluetooth_mac_address(self):
+        """
+        `str`: MAC address hex string
+
+        Property for the MAC address ID for bluetooth device
+        """
+        return ubinascii.hexlify(self.bt.config("mac")[1], ":")
+
+    def quit(self):
+        self.bt.active(False)
